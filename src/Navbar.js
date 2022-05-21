e646d24b976cbe7c4cb9fcc1bcb10d9bc97ab7a9
Vinay Yadav <vinayyadav91190@gmail.com>
2022-05-21 11:13:44 +0530

    create component folder for components

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h1> MovieStar</h1>
+                <h3> Favorite </h3>
+            </div>
+        )
+    }
+}
