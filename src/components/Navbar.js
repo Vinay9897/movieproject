6df7b9a40d87d07c93bdcc5681027ac618a99ebf
Vinay Yadav <vinayyadav91190@gmail.com>
2022-05-22 14:38:43 +0530

    Navbar complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,15 @@ import React, { Component } from 'react'
 export default class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h1> MovieStar</h1>
-                <h3> Favorite </h3>
+            <div className='container'>
+                <div className="header">
+                    <h1 className='spacing'> MOVIESTAR</h1>
+                    <ul className='items'>
+                        <li className='item'><a href='#Favorite'>Favourite</a></li>
+                        <li className='item'><a href='#About'>About</a></li>
+                        <li className='item'><a href='#About'>Contact</a></li>
+                    </ul>
+                </div>
             </div>
         )
     }
