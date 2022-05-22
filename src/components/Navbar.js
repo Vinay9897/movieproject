44df57e14fb2276cee42b00beef947180f05a4ee
Vinay Yadav <vinayyadav91190@gmail.com>
2022-05-22 22:06:14 +0530

    apply css on card clas

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default class Navbar extends Component {
         return (
             <div className='con'>
                 <div className="header">
-                    <h1 className='spacing'> MOVIESTAR</h1>
+                    <h1 className='spacing text'> MOVIESTAR</h1>
                     <ul className='items'>
                         <li className='item'><a href='#Favorite'>Favourite</a></li>
                         <li className='item'><a href='#About'>About</a></li>
