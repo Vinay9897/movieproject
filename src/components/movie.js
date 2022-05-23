b0693a07951f08f8a7303a834b1067583932129b
Vinay Yadav <vinayyadav91190@gmail.com>
2022-05-23 15:16:54 +0530

    create the hover effect on the add  button

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,6 +1,14 @@
 import { movies } from './getmovies';
 import React, { Component } from 'react'
+import { hover } from '@testing-library/user-event/dist/hover';
 export default class movie extends Component {
+    constructor() {
+        super();
+        this.state = {
+            hover: '',
+            page: [1]
+        }
+    }
     render() {
         let movie = movies.results;
         return (
@@ -15,13 +23,19 @@ export default class movie extends Component {
                             <div className='movies-list'>
                                 {
                                     movie.map((movieObj) => (
-                                        <div className="card movies-card">
+                                        <div className="card movies-card" onMouseEnter={() => this.setState({ hover: movieObj.id })}
+                                            onMouseLeave={() => this.setState({ hover: '' })} >
                                             <img src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`}
                                                 alt={movieObj.title} className="card-img-top movies-img" />
                                             <h4 className="card-title movies-title">
                                                 {movieObj.original_title}</h4>
+
+
                                             <div className='button-wrapper'>
-                                                <a href="#" className="btn btn-primary movies-btn">ADD</a>
+                                                {
+                                                    this.state.hover == movieObj.id &&
+                                                    <a href="#" className="btn btn-primary movies-btn">ADD</a>
+                                                }
                                             </div>
 
                                         </div>
@@ -32,10 +46,15 @@ export default class movie extends Component {
                                 <nav aria-label="Page navigation example">
                                     <ul className="pagination" >
                                         <li className="page-item"><a className="page-link" href="#">Previous</a></li>
-                                        <li className="page-item"><a className="page-link" href="#">1</a></li>
-                                        <li className="page-item"><a className="page-link" href="#">2</a></li>
-                                        <li className="page-item"><a className="page-link" href="#">3</a></li>
+                                        {
+                                            this.state.page.map((value) =>
+                                            (
+                                                <li className="page-item"><a className="page-link" href="#">1</a></li>
+                                            ))
+
+                                        }
                                         <li className="page-item"><a className="page-link" href="#">Next</a></li>
+
                                     </ul>
                                 </nav>
                             </div>
