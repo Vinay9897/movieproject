def8f5dee6ecef1242ece379bcbfe4b19505aee1
Vinay Yadav <vinayyadav91190@gmail.com>
2022-05-23 09:04:27 +0530

    adjust the view for mobile

--- src/components/movie.js
+++ src/components/movie.js
@@ -12,16 +12,16 @@ export default class movie extends Component {
                         </div> :
                         <div>
                             <h3 className='text-center'><strong>Trending</strong></h3>
-                            <div>
+                            <div className='movies-list'>
                                 {
                                     movie.map((movieObj) => (
                                         <div className="card movies-card">
                                             <img src={`https://image.tmdb.org/t/p/original${movieObj.backdrop_path}`}
-                                                alt={movieObj.title} className="card-img-top banner-img" />
-                                            <h1 className="card-title movie-title">
-                                                {movieObj.original_title}</h1>
-                                            <div className='buttom-wrapper'>
-                                                <a href="#" className="btn btn-primary ">Go somewhere</a>
+                                                alt={movieObj.title} className="card-img-top movies-img" />
+                                            <h4 className="card-title movies-title">
+                                                {movieObj.original_title}</h4>
+                                            <div className='button-wrapper'>
+                                                <a href="#" className="btn btn-primary movies-btn">ADD</a>
                                             </div>
 
                                         </div>
